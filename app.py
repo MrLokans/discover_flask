3f9b1811c69f2ce0acd2c72e5b1380c2b820aa18
MrLokans <trikster1911@gmail.com>
2016-03-02 20:43:44 +0300

    Login page and user authentication added.

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, request, url_for, session, redirect
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(32)
+
+USERNAME = 'admin'
+PASSWORD = 'password'
 
 
 @app.route('/')
@@ -12,6 +17,25 @@ def home():
 def welcome():
     return render_template("welcome.html")
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form.get('username', '')
+        password = request.form.get('password', '')
+        if username != USERNAME or password != PASSWORD:
+            error = 'Invalid username'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
     app.run(debug=True)
