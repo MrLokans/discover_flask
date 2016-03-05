d704e6d6261e79a19ae98982c10dbfa8d1c6a994
MrLokans <trikster1911@gmail.com>
2016-03-05 21:48:44 +0300

    Database check added.

--- app.py
+++ app.py
@@ -26,10 +26,13 @@ def login_required(func):
 @app.route('/')
 @login_required
 def home():
-    g.db = connect_db()
-    cur = g.db.execute('select * from posts')
-    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
-    g.db.close()
+    try:
+        g.db = connect_db()
+        cur = g.db.execute('select * from posts')
+        posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+        g.db.close()
+    except sqlite3.OperationalError:
+        flash("Database is not set up.")
     return render_template('index.html', posts=posts)
 
 
